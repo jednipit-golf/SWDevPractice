3e427bc26a14a16b8890bcd30466213f33f3f14c
jednipit-golf <jednipit_golf@hotmail.com>
2025-10-02 22:37:44 +0700

    plan project struct

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser=require('cookie-parser');
+const connectDB = require('./config/db');
+
+//Route files
+const authRouthes = require('./routes/authRoutes');
+
+//Load env vars
+dotenv.config({ path:'./config/config.env' });
+
+//Connect to Database
+connectDB();
+
+const app = express();
+
+//body parser
+app.use(express.json());
+
+//Cookie parser
+app.use (cookieParser());
+
+app.use('/api/v1/auth/', authRouthes);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections 
+process.on('unhandledRejection', (err,promise)=>{
+    console.log( `Error: ${err.message}` ); 
+    //Close server & exit process 
+    server.close(()=> process.exit(1));
+});
