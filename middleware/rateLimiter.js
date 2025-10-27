672dd762ec8c0fa4ccf673dfc05c995de7e710cf
jednipit-golf <jednipit_golf@hotmail.com>
2025-10-27 19:29:15 +0700

    fix rate limit

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -4,10 +4,10 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
   max: 100, // limit each IP to 100 requests per windowMs
-  message: {
-    success: false,
-    msg: 'Too many requests from this IP, please try again later.'
-  },
+  // message: {
+  //   success: false,
+  //   msg: 'Too many requests from this IP, please try again later.'
+  // },
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false,  // Disable the `X-RateLimit-*` headers
 });
