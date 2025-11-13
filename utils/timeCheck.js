699cfcb7bd012bb7a1686006a5582b42d4254fcf
jednipit-golf <jednipit_golf@hotmail.com>
2025-11-14 01:21:43 +0700

    fix: correct regex for time format validation

--- utils/timeCheck.js
+++ utils/timeCheck.js
@@ -1,7 +1,7 @@
 const validateTimeFormat = (apptTime) => {
     if (!apptTime) return false;
 
-    const timeRegex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
+    const timeRegex = /^([01][0-9]|2[0-3]):[0-5][0-9]$/;
     return timeRegex.test(apptTime);
 };
 
